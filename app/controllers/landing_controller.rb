1768c3ff0bfd9824dbdbacdcfcaa806ea0caf5b5
Conor Broderick <cjayjayb@gmail.com>
2017-01-02 18:21:35 +0000

    Can pull in user's followers now

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -2,6 +2,8 @@ class LandingController < ApplicationController
 	def callback
 		response = Instagram.get_access_token(params[:code], redirect_uri: ENV['REDIRECT_URI'])
 		session[:access_token] = response.access_token
-		redirect_to root_path
+		response = HTTParty.get('https://api.instagram.com/v1/users/self/followed-by?access_token=1191025.62f86cf.1e0a88ccf53e48b680d754beb05ea653')
+		@user_data = response['data']
+		# binding.pry
 	end
 end
