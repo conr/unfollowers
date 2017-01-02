a32ef285da04956e22b089a3b19deba2537260b0
Conor Broderick <cjayjayb@gmail.com>
2017-01-02 23:39:05 +0000

    Further progress

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -2,8 +2,11 @@ class LandingController < ApplicationController
 	def callback
 		response = Instagram.get_access_token(params[:code], redirect_uri: ENV['REDIRECT_URI'])
 		session[:access_token] = response.access_token
-		response = HTTParty.get('https://api.instagram.com/v1/users/self/followed-by?access_token=1191025.62f86cf.1e0a88ccf53e48b680d754beb05ea653')
-		@user_data = response['data']
-		# binding.pry
+		following_response = HTTParty.get("https://api.instagram.com/v1/users/self/follows?access_token=#{session[:access_token]}")
+		followers_response = HTTParty.get("https://api.instagram.com/v1/users/self/followed-by?access_token=#{session[:access_token]}")
+		@following_data = following_response['data']
+		@followers_data = followers_response['data']
+		@users_not_following_back = @following_data - @followers_data
+		# binding.pry 
 	end
 end
